c0b0abced9a5b59d3864fc474c5bfb4e3095f341
Onur <Onur@DESKTOP-EJCVO0P.local>
2022-10-28 01:56:22 +0300

    Degiskenler ve degiskenlerin kullanim ihtiyaclari

---
+++ src/variables/Main.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*System.out.println("Öğrenci Sayım : 12");
+		System.out.println("Öğrenci Sayım : 12");
+		System.out.println("Öğrenci Sayım : 12");
+		System.out.println("Öğrenci Sayım : 12");*/
+		
+		int ogrenciSayisi=13;
+		String mesaj="Öğrenci Sayısı :";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
