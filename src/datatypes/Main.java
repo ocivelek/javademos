bf9091f9174f5e3024475a8613b25b0c30e32f30
ocivelek <o.civelek5255@gmail.com>
2022-10-30 16:56:09 +0300

    dataTypes

--- src/datatypes/Main.java
+++ src/datatypes/Main.java
@@ -3,7 +3,12 @@ package datatypes;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//byte-integer 1 byte
+		//short-integer 2bytes
+		//int-integer 4bytes 
+		//long-integer 8 bytes
+		//boolean-True&false 1byte
+		//char tek tırnak içinde tek karakter
 
 	}
 
