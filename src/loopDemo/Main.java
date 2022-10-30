ba365e3097c9dabb5083009351d6d5399a439125
ocivelek <o.civelek5255@gmail.com>
2022-10-30 19:09:59 +0300

    whileLoop

--- src/loopDemo/Main.java
+++ src/loopDemo/Main.java
@@ -3,16 +3,24 @@ package loopDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		//FOR
-		int i;
-		for(i=1;i<10;i++) {
+		// FOR
+		// int i;
+		for (int i = 1; i < 10; i++) {
 			System.out.println(i);
 		}
 		System.out.println("Döngü Bitti");
-		for(i=0;i<=10;i+=2) {
+		for (int i = 0; i <= 10; i += 2) {
 			System.out.println(i);
 		}
 		System.out.println("Dögü Bitti");
+
+		// While
+		int i=1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While Döngüsü bitti");
 	}
 
 }
