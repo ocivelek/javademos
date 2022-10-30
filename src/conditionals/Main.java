158480ad7bbbdc99a1befc03cd3493b0ea369041
ocivelek <o.civelek5255@gmail.com>
2022-10-30 17:10:17 +0300

    conditionals - Kodlama-io

--- src/conditionals/Main.java
+++ src/conditionals/Main.java
@@ -3,8 +3,15 @@ package conditionals;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sayi=21;
+		if(sayi<20){
+			System.out.println("Girilen Sayı 20'den Küçük");
+		}else if(sayi==20) {
+			System.out.println("Sayı 20 ye eşittir");
+		}else {
+			System.out.println("Sayı 20 den büyüktür.");
+		}
+		
 	}
 
 }
