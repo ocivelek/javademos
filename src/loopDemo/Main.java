5e3c4a8fb17a9ecc3e803d520642866f7eb3d937
ocivelek <o.civelek5255@gmail.com>
2022-10-30 18:26:19 +0300

    forLoop

--- src/loopDemo/Main.java
+++ src/loopDemo/Main.java
@@ -3,8 +3,16 @@ package loopDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//FOR
+		int i;
+		for(i=1;i<10;i++) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+		for(i=0;i<=10;i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Dögü Bitti");
 	}
 
 }
