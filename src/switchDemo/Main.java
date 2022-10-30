d46b062d5eb8442187f01c5356ba5e2f3ac4ca89
ocivelek <o.civelek5255@gmail.com>
2022-10-30 18:08:57 +0300

    switchDemo

--- src/switchDemo/Main.java
+++ src/switchDemo/Main.java
@@ -3,8 +3,33 @@ package switchDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char grade = 'C';
+		switch (grade) {
+		case 'A':
+			System.out.println("5 Geçtiniz.");
+			break;
+		case 'B':
+			System.out.println("4 Geçtiniz.");
+			break;
+		case 'C':
+			System.out.println("3 Geçtiniz.");
+			break;
+		case 'D':
+			System.out.println("2 Geçtiniz.");
+			break;
+		case 'F':
+			System.out.println("1 Kaldınız.");
+			break;
+		default:
+			System.out.println("Geçersiz Not");
+			
+		  /*case 'B':
+		    case 'C':
+			System.out.println("3 Geçtiniz.");
+			break;*/ 
+			//iki durumuna ortak mesaj vermek
 
+		}
 	}
 
 }
