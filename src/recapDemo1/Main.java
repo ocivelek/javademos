41c1c8a93589b8f22087960d60dddd54578d6572
ocivelek <o.civelek5255@gmail.com>
2022-10-30 17:23:17 +0300

    recapDemo1

--- src/recapDemo1/Main.java
+++ src/recapDemo1/Main.java
@@ -3,8 +3,18 @@ package recapDemo1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sayi1=21;
+		int sayi2=2;
+		int sayi3=27;
+		int enBuyuk=sayi1;
+		
+		if(enBuyuk<sayi2) {
+			enBuyuk=sayi2;
+		}
+		if(enBuyuk<sayi3) {
+			enBuyuk=sayi3;
+		}
+		System.out.println("En Büyük = " + enBuyuk);
 	}
 
 }
