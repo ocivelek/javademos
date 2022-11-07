631e3ced6eb7a590c0491253bde90b2e59343b2e
ocivelek <o.civelek5255@gmail.com>
2022-11-07 23:32:49 +0300

    getter_setter ve encapsulation

--- src/classesWithAttributes_2_8/Main.java
+++ src/classesWithAttributes_2_8/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Product product = new Product();
-		product.id = 1;
-		product.name = "Laptop";
-		product.description = "Asus Laptop";
-		product.price = 4500;
-		product.stockAmount = 3;
-		ProductManager productManager=new ProductManager();
+		product.setId(1);
+		product.setName("Laptop");
+		product.setDescription("Asus Laptop");
+		product.setPrice(4500);
+		product.setStockAmount(3);
+		ProductManager productManager = new ProductManager();
 		productManager.Add(product);
+
+		System.out.println(product.getKod());
 	}
 
 }
--- src/classesWithAttributes_2_8/ProductManager.java
+++ src/classesWithAttributes_2_8/ProductManager.java
@@ -3,6 +3,6 @@ package classesWithAttributes_2_8;
 public class ProductManager {
 	public void Add(Product abc)/* Product classı */ {
 		// veri tabanı işlemleri
-		System.out.println("Ürün Eklendi. " + abc.name);
+		System.out.println("Ürün Eklendi. " + abc.getName());
 	}
 }
