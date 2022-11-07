425db7c53362c7532e85e074a530b54812281a1a
ocivelek <o.civelek5255@gmail.com>
2022-11-07 23:06:22 +0300

    classesWithAttributes_2_8

---
+++ src/classesWithAttributes_2_8/Product.java
@@ -0,0 +1,9 @@
+package classesWithAttributes_2_8;
+
+public class Product {
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+}
---
+++ src/classesWithAttributes_2_8/ProductManager.java
@@ -0,0 +1,8 @@
+package classesWithAttributes_2_8;
+
+public class ProductManager {
+	public void Add(Product abc)/* Product classı */ {
+		// veri tabanı işlemleri
+		System.out.println("Ürün Eklendi. " + abc.name);
+	}
+}
