8800e81dff58a352f3dab14d9f61a3e76315b3dc
ocivelek <o.civelek5255@gmail.com>
2022-11-12 16:23:27 +0300

    methodOverloading

---
+++ src/methodOverloading_2_9/Main.java
@@ -0,0 +1,11 @@
+package methodOverloading_2_9;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(1, 3));
+		System.out.println(dortIslem.topla(5, 6, 6));
+	}
+
+}
